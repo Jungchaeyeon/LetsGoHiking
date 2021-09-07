63be2214acb1f1437fd6aa01f88a10cc84dda9dd
chaeyeon <chaeyeon1697@gmail.com>
2021-09-07 18:07:43 +0900

    Update Key.kt

--- app/src/main/java/com/jcy/letsgohiking/Key.kt
+++ app/src/main/java/com/jcy/letsgohiking/Key.kt
@@ -1,11 +1,11 @@
 package com.jcy.letsgohiking
 
 object Key {
+    //자신이 등록받은 key로 
+    const val  TMAP_API = "TMAP_API"
 
-    const val  TMAP_API = "l7xxee38b53d2ddf4c6b943439b67e44eff9"
+    const val NAVER_API_CLIENT_ID ="NAVER_API_CLIENT_ID"
 
-    const val NAVER_API_CLIENT_ID ="Av1fZDBpnofuZHD7npGf"
-
-    const val NAVER_API_CLIENT_SECRET = "7UKKHa2Nzm"
+    const val NAVER_API_CLIENT_SECRET = "NAVER_API_CLIENT_SECRET"
 
 }
